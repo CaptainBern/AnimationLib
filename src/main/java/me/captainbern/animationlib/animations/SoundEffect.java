8589fcb9aeec6349fb315b7298d6d9ebdde5fc1c
CaptainBern <captainberneby@gmail.com>
2013-11-08 14:45:22 +0100

    Added SoundType class which contains all the text values of all known minecraft sounds.

--- src/main/java/me/captainbern/animationlib/animations/SoundEffect.java
+++ src/main/java/me/captainbern/animationlib/animations/SoundEffect.java
@@ -1,72 +1,81 @@
 package me.captainbern.animationlib.animations;
 
+import me.captainbern.animationlib.protocol.Packet;
+import me.captainbern.animationlib.protocol.PacketType;
+import me.captainbern.animationlib.utils.PlayerUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public enum SoundEffect {
 
-    NULL;
-	
-	/*AMBIENT_CAVE {
-		@Override
-		protected void broadcastAnimation(Location loc, float volume, int pitch) {
-			try{
-				
-				Packet packet = new Packet("Packet62NamedSoundEffect");
-				packet.setPrivateValue("a", "ambient.cave.cave");
-				packet.setPrivateValue("b", (int) loc.getX() * 8.0D);
-				packet.setPrivateValue("c", (int) loc.getY() * 8.0D);
-				packet.setPrivateValue("d", (int) loc.getZ() * 8.0D);
-				packet.setPrivateValue("e", volume);
-				pitch = (int) (pitch * 63.0F);
-				if(pitch < 0){
-					pitch = 0;
-				}
-				
-				if(pitch > 255){
-					pitch = 255;
-				}
-				
-				packet.setPrivateValue("f", pitch);
-	
-				sendPacketNearby(loc, Arrays.asList(packet));
+    AMBIENT_CAVE {
+        @Override
+        protected void broadcastAnimation(Location loc, float volume, int pitch) {
+            try{
 
-			}catch(Exception e){
-				Bukkit.getLogger().warning("[AnimationLib] Something went wrong while crafting the Packet62NamedSoundEffect packet! (BLOCK_BREAK)");
-				e.printStackTrace();
-			}
-		}
-	},
+                Packet packet = new Packet(PacketType.NAMED_SOUND_EFFECT);
+                packet.write("a", SoundType.AmbientCave);
+                packet.write("b", (int) loc.getY() * 8.0D);
+                packet.write("c", (int) loc.getY() * 8.0D);
+                packet.write("d", (int) loc.getZ() * 8.0D);
+                packet.write("e", volume);
+
+                pitch = (int) (pitch * 63.0F);
+                if(pitch < 0){
+                    pitch = 0;
+                }
+
+                if(pitch > 255){
+                    pitch = 255;
+                }
+
+                packet.write("f", pitch);
+
+                sendPacketNearby(loc, Arrays.asList(packet));
+
+            }catch(Exception e){
+                Bukkit.getLogger().warning("[AnimationLib] Something went wrong while crafting the Packet62NamedSoundEffect packet! (BLOCK_BREAK)");
+                e.printStackTrace();
+            }
+        }
+    },
 	AMBIENT_WEATHER_RAIN {
 		@Override
 		protected void broadcastAnimation(Location loc, float volume, int pitch) {
 			try{
-				
-				Packet packet = new Packet("Packet62NamedSoundEffect");
-				packet.setPrivateValue("a", "ambient.weather.rain");
-				packet.setPrivateValue("b", (int) loc.getX() * 8.0D);
-				packet.setPrivateValue("c", (int) loc.getY() * 8.0D);
-				packet.setPrivateValue("d", (int) loc.getZ() * 8.0D);
-				packet.setPrivateValue("e", volume);
-				pitch = (int) (pitch * 63.0F);
-				if(pitch < 0){
-					pitch = 0;
-				}
-				
-				if(pitch > 255){
-					pitch = 255;
-				}
-				
-				packet.setPrivateValue("f", pitch);
-	
-				sendPacketNearby(loc, Arrays.asList(packet));
+
+                Packet packet = new Packet(PacketType.NAMED_SOUND_EFFECT);
+                packet.write("a", SoundType.AmbientWeatherRain);
+                packet.write("b", (int) loc.getY() * 8.0D);
+                packet.write("c", (int) loc.getY() * 8.0D);
+                packet.write("d", (int) loc.getZ() * 8.0D);
+                packet.write("e", volume);
+
+                pitch = (int) (pitch * 63.0F);
+                if(pitch < 0){
+                    pitch = 0;
+                }
+
+                if(pitch > 255){
+                    pitch = 255;
+                }
+
+                packet.write("f", pitch);
+
+                sendPacketNearby(loc, Arrays.asList(packet));
 
 			}catch(Exception e){
 				Bukkit.getLogger().warning("[AnimationLib] Something went wrong while crafting the Packet62NamedSoundEffect packet! (BLOCK_BREAK)");
 				e.printStackTrace();
 			}
 		}
-	},
-	AMBIENT_WEATHER_THUNDER {
+	},;
+	/*AMBIENT_WEATHER_THUNDER {
 		@Override
 		protected void broadcastAnimation(Location loc, float volume, int pitch) {
 			try{
@@ -489,24 +498,24 @@ public enum SoundEffect {
 
 	/* STOP ENUMS */
 
-	/*public void play(Location loc, float volume, int pitch){
-		broadcastAnimation(loc, pitch, pitch);
-	}
+    public void play(Location loc, float volume, int pitch){
+        broadcastAnimation(loc, pitch, pitch);
+    }
 
-	private static void sendPacketNearby(Location loc, Collection<Packet> packets) {
-		World world = loc.getWorld();
-		for(Player player : Bukkit.getOnlinePlayers()){
-			if(player == null || player.getWorld() != world){
-				continue;
-			}
-				for(Packet packet : packets){
-					packet.send(player);
-			}
-		}
-	}  */
+    private static void sendPacketNearby(Location loc, Collection<Packet> packets) {
+        World world = loc.getWorld();
+        for(Player player : Bukkit.getOnlinePlayers()){
+            if(player == null || player.getWorld() != world){
+                continue;
+            }
+            for(Packet packet : packets){
+                PlayerUtil.sendPacket(player, packet);
+            }
+        }
+    }
 
-	protected void broadcastAnimation(Location loc, float volume, int pitch){
-		throw new UnsupportedOperationException("[AnimationLib] Unimplemented animation");
-	}
+    protected void broadcastAnimation(Location loc, float volume, int pitch){
+        throw new UnsupportedOperationException("[AnimationLib] Unimplemented animation");
+    }
 
 }
