0aa5a8fe4890f1de4794bf0e9424a26f47395a07
CaptainBern <captainberneby@gmail.com>
2013-12-01 18:31:02 +0100

    Updated imports.

--- src/main/java/me/captainbern/animationlib/utils/refs/PacketRef.java
+++ src/main/java/me/captainbern/animationlib/utils/refs/PacketRef.java
@@ -1,11 +1,9 @@
 package me.captainbern.animationlib.utils.refs;
 
 import com.google.common.collect.BiMap;
+
 import me.captainbern.animationlib.reflection.ClassTemplate;
-import me.captainbern.animationlib.reflection.FieldAccessor;
 import me.captainbern.animationlib.reflection.NMSClassTemplate;
-import me.captainbern.animationlib.reflection.SafeField;
-import me.captainbern.animationlib.utils.wrappers.IntHashMap;
 
 public class PacketRef {
 
