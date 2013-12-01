630ef783507f3332ca23a6e7e148c5346fe75296
CaptainBern <captainberneby@gmail.com>
2013-12-01 20:48:45 +0100

    Fixed stuff

--- src/main/java/me/captainbern/animationlib/utils/refs/PacketRef.java
+++ src/main/java/me/captainbern/animationlib/utils/refs/PacketRef.java
@@ -10,6 +10,6 @@ public class PacketRef {
     public static final ClassTemplate<Object> protocol = NMSClassTemplate.create("EnumProtocol");
 
     public static BiMap getServerPacketRegistry(){
-       return (BiMap) protocol.getField("h").get(protocol.newInstance());
+       return (BiMap) protocol.getMethod("a").invoke(protocol.newInstance());
     }
 }
