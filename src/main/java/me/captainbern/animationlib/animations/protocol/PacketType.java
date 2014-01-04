d91c1c91ffa8185a1d21653a06a52a0a07408e66
CaptainBern <captainberneby@gmail.com>
2014-01-04 21:22:43 +0100

    Removed the mess.

--- src/main/java/me/captainbern/animationlib/animations/protocol/PacketType.java
+++
@@ -1,4 +0,0 @@
-package me.captainbern.animationlib.animations.protocol;
-
-public class PacketType {
-}
--- src/main/java/me/captainbern/animationlib/animations/protocol/Sender.java
+++
@@ -1,8 +0,0 @@
-package me.captainbern.animationlib.animations.protocol;
-
-public enum Sender {
-
-    CLIENT,
-    SERVER;
-
-}
